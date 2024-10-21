b5dc89280d95a6075c6e96820bd61dcc543b31a3
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-21 22:38:56 +0600

    added components and fake data

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-       <h1 className='text-6xl bg-red-300'>KnowLadge Cafe</h1>
+      
+     <Header></Header>
+     <Blogs></Blogs>
+    
+     
     </>
   )
 }
