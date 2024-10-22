b0fe4ceda316a1a7deaffb75079da7893c356ef1
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-22 12:20:20 +0600

    added blog

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,44 @@
+import { space } from 'postcss/lib/list';
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const Blog = ({blog}) => {
+    const {title,cover,author_img,author,reading_time,posted_date,
+        hashtags
+    }= blog;
+    return (
+        <div>
+            <img src={cover} alt={`cover picture of title ${title}`}  ></img>
+            <div className='flex justify-between '>
+
+                <div className='flex'>
+                      <img className='w-14' src={author_img} alt="" />
+                      <div className='ml-6'>
+                         <h3 className='text-2xl'>{author}</h3>
+                         <p>{posted_date}</p>
+                      </div>
+
+                </div>
+
+                <div>
+
+                    <span>{reading_time} min read </span>
+
+                </div>
+
+            </div>
+           
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash,idx)=> <span key={idx}> <a href=''> #{hash} </a>   </span> )
+                }
+            </p>
+        </div>
+    );
+};
+Blog.propTypes={
+    blog: PropTypes.object.isRequired
+}
+
+export default Blog;
