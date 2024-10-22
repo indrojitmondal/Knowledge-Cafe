4c7a79914b6e6bb0ad35d0aa6ca97cffc70af271
indroji-mondal <indro.cse.bu@gmail.com>
2024-10-22 11:29:58 +0600

    added blogs

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
+
 
 
 
@@ -14,7 +16,14 @@ function App() {
     <>
       
      <Header></Header>
-     <Blogs></Blogs>
+     
+
+     <div className='md:flex'>
+      <Blogs>
+
+      </Blogs>
+      <Bookmarks></Bookmarks>
+     </div>
     
      
     </>
